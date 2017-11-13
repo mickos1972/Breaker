8d0152fc2b09dc1fb2c227219cc54099089f01ab
mick murphy <mickm15@hotmail.com>
2017-11-13 19:22:33 +0000

    Re-formatted Code

--- Breaker/GameViewController.swift
+++ Breaker/GameViewController.swift
@@ -1,30 +1,40 @@
 import UIKit
 import SceneKit
-class GameViewController: UIViewController {
+class GameViewController: UIViewController
+{
     var scnView: SCNView!
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // 1
+        
         setupScene()
         setupNodes()
         setupSounds()
     }
-    // 2
-    func setupScene() {
+    
+    func setupScene()
+    {
         scnView = self.view as! SCNView
         scnView.delegate = self
     }
-    func setupNodes() {
+    
+    func setupNodes()
+    {
     }
-    func setupSounds() {
+    
+    func setupSounds()
+    {
     }
+    
     override var shouldAutorotate: Bool { return true }
     override var prefersStatusBarHidden: Bool { return true }
 }
-// 3
-extension GameViewController: SCNSceneRendererDelegate {
-    func renderer(_ renderer: SCNSceneRenderer,
-                  updateAtTime time: TimeInterval) {
+
+extension GameViewController: SCNSceneRendererDelegate
+{
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval)
+    {
     }
 }
 
